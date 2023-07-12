e141423696688df6a9eb728ec1939d625fe472c4
SONU KOL <108781856+LinkNatureAi@users.noreply.github.com>
2023-07-12 20:22:13 +0530

    Update google-search-train-restful-api.js

--- google-search-train-restful-api.js
+++ google-search-train-restful-api.js
@@ -3,17 +3,18 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const app = express();
-const port = process.env.PORT || 3330;
+const port = process.env.PORT || 3300;
 
-app.get('/:num', async (req, res) => {
+app.get('/', async (req, res) => {
   try {
-    const { num } = req.params;
-    const url = `https://www.google.com/search?q=${num}+train+running+status`;
-    const { data } = await axios.get(url, { 
+    const url = `https://www.google.com/search?q=18234+train+running+status`;
+    const { data } = await axios.get(url, {
       headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-      }
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+      },
     });
+
     const modifiedData = modifyJSON(data);
     res.json(modifiedData);
   } catch (error) {
@@ -28,9 +29,9 @@ app.listen(port, () => {
 
 function modifyJSON(data) {
   const $ = cheerio.load(data);
-  const trainName = $('div.k9rLYb').eq(0).text().trim();
-  const liveStatus = $('div.dK1Bub .rUtx7d').eq(1).text();//.replace('Est arrival', 'pahuch ne wali').replace('Est departure', 'pahuchi thi');
+  const trainName = $('div .ROYhg div').text().trim();
+  const liveStatus = $('div.dK1Bub .rUtx7d').eq(1).text();
   const delayTime = $('div.Rjvkvf.MB86Dc').eq(1).text().trim();
-  
+
   return { trainName, liveStatus, delayTime };
 }
